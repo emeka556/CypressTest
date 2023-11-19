ceb914be3ad51a35e57cc7c3098e0693ebd64794
emeka556 <emekaokonji@live.com>
2023-11-19 16:24:10 +0000

    Login Features

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,29 @@
+//import {data} from "cypress"
+import Login_PO from "../support/Pages/Login"
+
+describe('Login', () =>{
+  const login_PO = new Login_PO
+  beforeEach(function(){ 
+cy.fixture('credentials').then((data)=>{
+  globalThis.data = data
+
+})
+
+  })
+
+  it('successful login', () =>{
+    expect(data.login.validdataset).to.exist
+      login_PO.Login(data.login.validdataset.username, data.login.validdataset.password)
+      login_PO.validAssertion
+      
+
+    }) 
+    it('unsuccessful login', () =>{
+      expect(data.login.invaliddataset).to.exist
+       login_PO.Login(data.login.invaliddataset.username, data.login.invaliddataset.password)
+       login_PO.invalidAssertion
+       
+
+    })
+
+})
---
+++ cypress/support/Pages/Login.js
@@ -0,0 +1,31 @@
+class Login_PO {
+    Login(username, password) {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
+        cy.get('.oxd-button').click();
+
+    
+    }
+
+    validAssertion() {
+        cy.url().should("eq", "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+        cy.contains("Dashboard").should("be.visible")
+        cy.contains("My Info").should("be.visible")
+        cy.waitUntil(() => cy.title().should('include', 'dashboard'))
+
+    }
+
+    invalidAssertion() {
+        cy.get('.oxd-alert-content').should("be.visible")
+        cy.contains("Invalid credentials").should("be.visible")
+    }
+
+    waitAfterAssertion() {
+        cy.wait(5000) // wait 5 seconds until the next block of code
+    }
+
+    
+}
+
+export default Login_PO;
