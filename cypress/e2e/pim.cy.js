849b4002b254ab8a097ac0d881d0191f786a74d3
emeka556 <emekaokonji@live.com>
2023-11-22 11:05:25 +0000

    PIM feature implemented

---
+++ cypress/e2e/pim.cy.js
@@ -0,0 +1,24 @@
+import Login_PO from "../support/Pages/Login"
+import PIM from "../support/Pages/PIM"
+
+describe('PIM', () =>{
+    const login_PO = new Login_PO
+    const pIM = new PIM
+    
+    before(function(){ 
+  cy.fixture('credentials').then((data)=>{
+    globalThis.data = data
+  
+  })
+  
+    })
+
+
+it('successful login', () =>{
+    
+    login_PO.Login(data.login.validdataset.username, data.login.validdataset.password)
+    pIM.clickPIM()
+    
+
+    }) 
+})
---
+++ cypress/support/Pages/PIM.js
@@ -0,0 +1,14 @@
+
+
+class PIM{
+
+    clickPIM(){
+       
+        cy.get(':nth-child(2) > .oxd-main-menu-item > .oxd-text').click()
+
+            }
+
+
+}
+
+export default PIM;
